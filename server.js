585576ddd2d06e42128a54013ae45fbf887b64e4
Glitch (assessment-schedule-webapi) <none>
2020-08-21 07:45:46 +0000

    🍯👨‍❤️‍💋‍👨 Updated with Glitch

--- server.js
+++ server.js
@@ -102,6 +102,9 @@ app.get('/api/K61_0', (req, res) => {
             return;
         }
       
+        var param_date_yMd = param_date.substr(0,4) + param_date.substr(4,2) + param_date.substr(6,2);
+        var param_ts_yMd = param_timestamp.substr(0,4) + param_timestamp.substr(4,2) + param_timestamp.substr(6,2);
+      
         var param_date_yyyy = param_date.substr(0,4);
         //console.log(yyyy);
         var param_date_MM = param_date.substr(4,2);
@@ -109,6 +112,76 @@ app.get('/api/K61_0', (req, res) => {
         var param_date_dd = param_date.substr(6,2);
         //console.log(dd);
 
+        // 今日の情報を要求された時
+        if (param_date_yMd == param_ts_yMd)
+        {
+            const assessment_schedule_now = 
+                {
+                    "status": 200,
+                    "assessment_schedule_total": 3,
+                    "assessment_schedule_data": [
+                        {
+                            "assessment_schedule_no": 1,
+                            "business_discussion_id": 20255571,
+                            "startdate": param_date_yyyy + "%2F" + param_date_MM + "%2F" + param_date_dd + "+07%3A00%3A00",
+                            "enddate": param_date_yyyy + "%2F" + param_date_MM + "%2F" + param_date_dd + "+09%3A30%3A00",
+                            "customer_name": "%E7%B9%94%E7%94%B0+%E4%BF%A1%E9%95%B7",
+                            "tel": "09011111111",
+                            "zip_code": "111-1111",
+                            "address": "%E6%9D%B1%E4%BA%AC%E9%83%BD%E5%A2%A8%E7%94%B0%E5%8C%BA%E6%8A%BC%E4%B8%8A1-1-2",
+                            "model_year": "H30",
+                            "car_name": "%EF%BC%91%E3%82%B7%E3%83%AA%E3%83%BC%E3%82%BA",
+                            "model": "DBA-1A16",
+                            "grade": "%EF%BC%91%EF%BC%91%EF%BC%96%EF%BD%89+%E3%82%B9%E3%82%BF%E3%82%A4%E3%83%AB",
+                            "total_distance": 26565,
+                            "confirm_note": "%E9%A7%90%E8%BB%8A%E5%A0%B4%E9%81%A0%E3%81%84",
+                            "contact_information": "%EF%BC%A2%EF%BC%91%E6%9F%BB",
+                            "business_discussion_result": ""
+                        },
+                        {
+                            "assessment_schedule_no": 2,
+                            "business_discussion_id": 20255639,
+                            "startdate": param_date_yyyy + "%2F" + param_date_MM + "%2F" + param_date_dd + "+11%3A30%3A00",
+                            "enddate": param_date_yyyy + "%2F" + param_date_MM + "%2F" + param_date_dd + "+15%3A30%3A00",
+                            "customer_name": "%E8%B1%8A%E8%87%A3+%E7%A7%80%E5%90%89",
+                            "tel": "09022222222",
+                            "zip_code": "222-2222",
+                            "address": "%E6%9D%B1%E4%BA%AC%E9%83%BD%E6%96%B0%E5%AE%BF%E5%8C%BA%E5%86%85%E8%97%A4%E7%94%BA%EF%BC%91%EF%BC%91",
+                            "model_year": "R02",
+                            "car_name": "%E3%83%91%E3%83%AC%E3%83%83%E3%83%88",
+                            "model": "CBA-MK21S",
+                            "grade": "%EF%BC%B4",
+                            "total_distance": 753,
+                            "confirm_note": "",
+                            "contact_information": "A1",
+                            "business_discussion_result": ""
+                        },
+                        {
+                            "assessment_schedule_no": 3,
+                            "business_discussion_id": 20255752,
+                            "startdate": param_date_yyyy + "%2F" + param_date_MM + "%2F" + param_date_dd + "+17%3A00%3A00",
+                            "enddate": param_date_yyyy + "%2F" + param_date_MM + "%2F" + param_date_dd + "+20%3A30%3A00",
+                            "customer_name": "%E5%BE%B3%E5%B7%9D+%E5%AE%B6%E5%BA%B7",
+                            "tel": "09033333333",
+                            "zip_code": "333-3333",
+                            "address": "%E6%9D%B1%E4%BA%AC%E9%83%BD%E5%A2%A8%E7%94%B0%E5%8C%BA%E6%A8%AA%E7%B6%B2%EF%BC%91%E4%B8%81%E7%9B%AE%EF%BC%94%E2%88%92%EF%BC%91",
+                            "model_year": "S53",
+                            "car_name": "%E3%82%B8%E3%83%BC%E3%83%97%E3%83%BB%E3%83%A9%E3%83%B3%E3%82%B0%E3%83%A9%E3%83%BC%E3%82%A2%E3%83%B3%E3%83%AA%E3%83%9F%E3%83%86%E3%83%83%E3%83%89",
+                            "model": "ABA-JK36L",
+                            "grade": "%E3%83%AB%E3%83%93%E3%82%B3%E3%83%B3+%E3%83%8F%E3%83%BC%E3%83%89%E3%83%AD%E3%83%83%E3%82%AF",
+                            "total_distance": 102537,
+                            "confirm_note": "%E5%9C%B0%E4%B8%8B2%EF%BC%A6",
+                            "contact_information": "",
+                            "business_discussion_result": ""
+                        }
+                    ]
+                }
+            ;
+            // JSONを送信する
+            res.json(assessment_schedule_now);
+            return;
+        }
+      
         // status ok 2件
         const assessment_schedule = 
             {
